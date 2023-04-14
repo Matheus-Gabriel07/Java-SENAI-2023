67daa71951dbe70e802e3196ba3b04c1292ae0c8
Matheus-Gabriel07 <matheus.gssilva19@gmail.com>
2023-04-14 10:44:52 -0300

    Summary

---
+++ Exercícios/Atividade3.java
@@ -0,0 +1,19 @@
+public class Atividade3 {
+    public static void main(String[] args) {
+        String nome = "Matheus Gabriel";
+        String nomeCrush = "Não tenho";
+        String endereco = "Rua do Amor";
+        int idade = 15;
+        char estadoCivil = 'S';
+        double altura = 1.83;
+        int telefone = 44028922;
+
+        System.out.println("Nome: "+nome);
+        System.out.println("Nome do crush: "+nomeCrush);
+        System.out.println("Endereço: "+endereco);
+        System.out.println("Meu estado civil: "+estadoCivil);
+        System.out.println("Idade: "+idade+" anos");
+        System.out.println("Altura: "+altura+" metros");
+        System.out.println("Meu telefone: "+telefone);
+    }
+}
