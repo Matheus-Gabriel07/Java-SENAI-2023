ad279b0bd466cba265d0e7d4b39e4bac5c3d3ec2
Matheus-Gabriel07 <matheus.gssilva19@gmail.com>
2023-04-26 16:03:38 -0300

    Update

--- Aula03/Atividade05.java
+++ Aula03/Atividade05.java
@@ -10,17 +10,21 @@ public class Atividade05{
         int salarioMini = 1311;
         double alturaMaior = (int) 1.8;
         int pesoMenor = 60;
-        String nomeMeu = "Matheus Gabriel Santos Silva";
         String generoMeu = "m";
 
         //Váriais de scanner
         int idade, salario, peso;
         double altura;
-        String nome, genero;
 
         //Pedindo as informações
-        System.out.println("Qual o seu nome");
-        nome = sc.nextLine();
+
+        System.out.println("Digite o nome padrão: ");
+        String nome = sc.nextLine();
+        System.out.println("Digite seu nome: ");
+        String meuNome = sc.nextLine();
+        System.out.println("Digite seu genero: ");
+        String genero = sc.nextLine();
+
         System.out.println("Qual a sua idade: ");
         idade = sc.nextInt();
         System.out.println("Qual o seu salário: ");
@@ -29,17 +33,23 @@ public class Atividade05{
         altura = sc.nextDouble();
         System.out.println("Qual o seu peso: ");
         peso = sc.nextInt();
-        System.out.println("Qual o seu genero: ");
-        genero = sc.next();
+      
+        //Aplicação da igualdade de Strings
+        boolean iguais =nome.equals(meuNome);
+        boolean semelhante = generoMeu.equals(genero);
+
+        String mensagem = (iguais==true)?"True": "False";
+        String mensagem2 = (semelhante==true)?"True": "False";
+
 
         //Informações verdadeiras
-        System.out.println("========");
-        System.out.println("Seu nome é igual ao meu? "+(nome==nomeMeu));
+        System.out.println("========");//Separar
+        System.out.println("Seu nome é igual ao meu? "+(mensagem));
+        System.out.println("Seu gênero é m? "+(mensagem2));
         System.out.println("Você é de maior? "+(idade>=idadeDeMaior));
         System.out.println("Você ganha mais que o salário minimo? "+(salario>=salarioMini));
         System.out.println("Você é alto? "+(altura>=alturaMaior));
         System.out.println("Você é leve "+(peso<pesoMenor));
-        System.out.println("Seu gênero é m? "+(generoMeu==genero));
         sc.close();
     }
 }
