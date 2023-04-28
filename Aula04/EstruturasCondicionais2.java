bedaff246fe8090d9efba7c4d02617d21fc61e94
Matheus-Gabriel07 <matheus.gssilva19@gmail.com>
2023-04-28 09:52:14 -0300

    Update

--- Aula04/EstruturasCondicionais2.java
+++ Aula04/EstruturasCondicionais2.java
@@ -5,6 +5,34 @@ import java.util.Scanner;
 public class EstruturasCondicionais2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+
+        System.out.println("Digite seu salário:");
+        double salario = sc.nextDouble();
+
+        System.out.println("Digite sua idade:");
+        int idade = sc.nextInt();
+
+        //Valores padrão
+        int idadeDeMaior = 18;
+        double salarioMinimo = 1320;
+
+        if (salario>salarioMinimo && idade>=idadeDeMaior) {
+            System.out.println("Você é maior de idade e ganha mais que o salário mínimo");
+        } 
+        if (salario<salarioMinimo && idade>=idadeDeMaior){
+            System.out.println("Você é maior de idade e ganha menos que o salário minímo");
+        }
+        if (salario==salarioMinimo && idade>=idadeDeMaior){
+            System.out.println("Você é maior de idade e ganha um salário minímo");
+        }
+        if (salario>salarioMinimo && idade<idadeDeMaior){
+            System.out.println("Você é menor de idade e ganha mais que o salário minímo");
+        }
+        if (salario<salarioMinimo && idade<idadeDeMaior){
+            System.out.println("Você é menor de idade e ganha menos que um salário minímo");
+        }
+        if (salario==salarioMinimo && idade<idadeDeMaior){
+            System.out.println("Você é menor de idade e ganha um salário minímo");
+        }
     }
 }
