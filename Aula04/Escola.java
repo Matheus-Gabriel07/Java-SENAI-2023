14a103fce44a7c8ea523ffb60b30037cf34396cc
Matheus-Gabriel07 <matheus.gssilva19@gmail.com>
2023-04-28 11:28:18 -0300

    Update

--- Aula04/Escola.java
+++ Aula04/Escola.java
@@ -2,14 +2,12 @@ package Aula04;
 
 import java.util.Scanner;
 
-import org.omg.CORBA.ARG_OUT;
-
 public class Escola { //Esse programa cadastra alunos via scanner
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         //Entrada de dados
-        System.out.println("==Cadastro do aluno==");
+        System.out.println("== Cadastro do aluno ==");
         System.out.println("Digite o nome do aluno:");
         String nomeAluno = sc.nextLine();
 
@@ -26,10 +24,10 @@ public class Escola { //Esse programa cadastra alunos via scanner
         int faltas = sc.nextInt();
 
         //Condicionais
-        if (media>=5.1 && faltas<4){
+        if ((media>=5.1 && media<=10) && faltas<4){
             System.out.println("O alunos "+nomeAluno+" está aprovado");
         }
-        if (media==5 && faltas>=5 && faltas<19){
+        if (media==5 && (faltas>=5 && faltas<=19)){
             System.out.println("O alunos "+nomeAluno+" está de recuperação, entre em contato "+tel);
         }
         if (media<5 && faltas>=20){
