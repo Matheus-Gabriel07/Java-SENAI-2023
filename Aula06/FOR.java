ef349fcf5b3ffb7cfb06836231adf0fecbe9a9b3
Matheus-Gabriel07 <matheus.gssilva19@gmail.com>
2023-05-10 10:40:00 -0300

    Create FOR.java

---
+++ Aula06/FOR.java
@@ -0,0 +1,25 @@
+package Aula06;
+
+public class FOR {
+    public static void main(String[] args) {
+        for (int i = 1; i<=100; i++){
+            if (i%2==0 && i%3==0){
+                System.out.println(i+" esse número é par e multiplo de 3");
+            }else if (i%2!=0 && i%3==0) {
+                System.out.println(i+" esse número é impar e multiplo de 3");
+            }
+
+            if (i%2==0 && i%4==0){
+                System.out.println(i+" esse número é par e multiplo de 4");
+            }else if (i%2!=0 && i%4==0) {
+                System.out.println(i+" esse número é impar e multiplo de 4");
+            }
+
+            if (i%2==0 && i%5==0){
+                System.out.println(i+" esse número é par e multiplo de 5");
+            }else if (i%2!=0 && i%5==0) {
+                System.out.println(i+" esse número é impar e multiplo de 5");
+            }
+        }
+    }
+}
